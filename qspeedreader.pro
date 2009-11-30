232252d0bc9153cdbb15aea8427d1e9b52d386e9
Christian Balles <code@ballessay.de>
2009-11-30 02:14:06 +0100

    added licence information

--- qspeedreader.pro
+++ qspeedreader.pro
@@ -9,19 +9,27 @@ MOC_DIR = .moc
 UI_DIR = .ui
 RCC_DIR = .rcc
 SOURCES += main.cpp \
-           mainwindow.cpp \
-           settings.cpp \
-           colordialog.cpp
+    mainwindow.cpp \
+    settings.cpp \
+    colordialog.cpp
 HEADERS += mainwindow.h \
-           settings.h \
-           colordialog.h
+    settings.h \
+    colordialog.h
 FORMS += mainwindow.ui \
-         colordialog.ui
-OTHER_FILES += README
+    colordialog.ui
+OTHER_FILES += README \
+    LICENCE
 TRANSLATIONS = lang/qspeedreader_en.ts \
-               lang/qspeedreader_de.ts
+    lang/qspeedreader_de.ts
+
 # switch on extra compiler stuff
-QMAKE_CXXFLAGS=-ansi -Wall
-QMAKE_CXXFLAGS_RELEASE=-mmmx -msse -Os
-QMAKE_LFLAGS = -ansi -Wall
-QMAKE_LFLAGS_RELEASE=-mmmx -msse -Os
+QMAKE_CXXFLAGS = -ansi \
+    -Wall
+QMAKE_CXXFLAGS_RELEASE = -mmmx \
+    -msse \
+    -Os
+QMAKE_LFLAGS = -ansi \
+    -Wall
+QMAKE_LFLAGS_RELEASE = -mmmx \
+    -msse \
+    -Os
--- settings.cpp
+++ settings.cpp
@@ -1,3 +1,23 @@
+/*
+| author:   Christian Balles
+| email:    code@ballessay.de
+| date:     2009/11/28
+| version:  0.1
+| copyright 2009 Christian Balles
+|
+|   This file is part of qspeedreader.
+|   qspeedreader is free software: you can redistribute it and/or modify it
+|   under the terms of the GNU General Public License as published by the Free
+|   Software Foundation, either version 2 of the License, or (at your option)
+|   any later version.
+|   qspeedreader is distributed in the hope that it will be useful, but
+|   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+|   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+|   more details.
+|   You should have received a copy of the GNU General Public License along with
+|   qspeedreader. If not, see http://www.gnu.org/licenses/.
+*/
+
 #include "settings.h"
 
 #define DEF_CONST(str) static const char* c_##str = #str
