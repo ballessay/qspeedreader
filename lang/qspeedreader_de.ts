105bc9a02a430aded02e35cd61bac8612c0ec168
Christian Balles <code@ballessay.de>
2009-11-30 01:19:57 +0100

    added german translation

--- lang/qspeedreader_de.ts
+++ lang/qspeedreader_de.ts
@@ -6,23 +6,23 @@
     <message>
         <location filename="../colordialog.ui" line="14"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Farbauswahl</translation>
     </message>
     <message>
         <location filename="../colordialog.ui" line="20"/>
         <source>Foreground:</source>
-        <translation type="unfinished"></translation>
+        <translation>Vordergrund:</translation>
     </message>
     <message>
         <location filename="../colordialog.ui" line="27"/>
         <location filename="../colordialog.ui" line="41"/>
         <source>Color</source>
-        <translation type="unfinished"></translation>
+        <translation>Farbe</translation>
     </message>
     <message>
         <location filename="../colordialog.ui" line="34"/>
         <source>Background:</source>
-        <translation type="unfinished"></translation>
+        <translation>Hintergrund:</translation>
     </message>
 </context>
 <context>
@@ -30,110 +30,110 @@
     <message>
         <location filename="../mainwindow.ui" line="14"/>
         <source>QSpeedReader</source>
-        <translation type="unfinished"></translation>
+        <translation>QSpeedReader</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="43"/>
         <source>Copy the text to clipboard (e.g. ctrl + c), adjust the settings to your needs and press start</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopieren Sie den Text in die Zwischenablage (z. B. mit Strg + c), passen Sie die Einstellungen an und drücken sie den &quot;Start&quot;-Knopf</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="85"/>
         <location filename="../mainwindow.ui" line="101"/>
         <source>Set speed</source>
-        <translation type="unfinished"></translation>
+        <translation>Geschwindigkeit einstellen</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="88"/>
         <source>Words per minute:</source>
-        <translation type="unfinished"></translation>
+        <translation>Wörter pro Minute:</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="104"/>
         <source> wpm</source>
-        <translation type="unfinished"></translation>
+        <translation> wpm</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="123"/>
         <source>Select font</source>
-        <translation type="unfinished"></translation>
+        <translation>Schrift auswählen</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="126"/>
         <source>Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Schrift</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="133"/>
         <source>Select colors</source>
-        <translation type="unfinished"></translation>
+        <translation>Farben auswählen</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="136"/>
         <source>Colors</source>
-        <translation type="unfinished"></translation>
+        <translation>Farben</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="143"/>
         <source>Start speed reading (ctrl + r)</source>
-        <translation type="unfinished"></translation>
+        <translation>Speedreading starten (Strg + r)</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="146"/>
         <location filename="../mainwindow.cpp" line="67"/>
         <location filename="../mainwindow.cpp" line="129"/>
         <source>Start</source>
-        <translation type="unfinished"></translation>
+        <translation>Start</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="156"/>
         <source>Repeat running</source>
-        <translation type="unfinished"></translation>
+        <translation>Nicht am Ende anhalten</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="159"/>
         <source>Repeat</source>
-        <translation type="unfinished"></translation>
+        <translation>wiederholen</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="207"/>
         <source>Reader</source>
-        <translation type="unfinished"></translation>
+        <translation>Reader</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="219"/>
         <source>Fullscreen</source>
-        <translation type="unfinished"></translation>
+        <translation>Vollbild</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="222"/>
         <source>Ctrl+F</source>
-        <translation type="unfinished"></translation>
+        <translation>Strg+f</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="227"/>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>Beenden</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="230"/>
         <source>Ctrl+Q</source>
-        <translation type="unfinished"></translation>
+        <translation>Strg+q</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="235"/>
         <source>Run</source>
-        <translation type="unfinished"></translation>
+        <translation>Start</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="238"/>
         <source>Ctrl+R</source>
-        <translation type="unfinished"></translation>
+        <translation>Strg+r</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="59"/>
         <source>Stop</source>
-        <translation type="unfinished"></translation>
+        <translation>Stop</translation>
     </message>
 </context>
 </TS>
